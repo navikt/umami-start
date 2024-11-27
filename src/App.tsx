5c14e2500c32eb0d13208a9a494656e575801b0d
EIlif Johansen <eilifjohansen@outlook.com>
2024-11-27 13:20:31 +0100

    La til intro tekst

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import TeamWebsites from "./components/teamwebsites.tsx";
-import {Page, InternalHeader, Heading} from "@navikt/ds-react";
+import {Page, InternalHeader, Heading, BodyShort, Link} from "@navikt/ds-react";
 
 function App() {
 
@@ -10,7 +10,11 @@ function App() {
                 <InternalHeader.Title as="h1">Start Umami</InternalHeader.Title>
             </InternalHeader>
             <Page.Block as="main" width="xl" gutters>
-                <Heading spacing={true} size="large" style={{ marginTop: "60px" }}>Nettsider</Heading>
+                <Heading spacing={true} as="h2" size="large" style={{ marginTop: "60px" }}>Nettsider</Heading>
+                <BodyShort size="medium" style={{ marginBottom: "60px" }}>
+                    Kontakt <Link target="_blank" href={`https://teamkatalog.nav.no/team/26dba481-fd96-40a8-b47d-b1ad0002bc74`}>Team ResearchOps</Link> for å få sporingskode til din nettside.
+                </BodyShort>
+
                 <TeamWebsites/>
             </Page.Block>
         </Page>
