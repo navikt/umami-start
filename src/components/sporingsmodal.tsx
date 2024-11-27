1aea39c7111d59454c3699d7706238fb0dd5b20b
EIlif Johansen <eilifjohansen@outlook.com>
2024-11-27 15:24:38 +0100

    La til modal

--- src/components/sporingsmodal.tsx
+++ src/components/sporingsmodal.tsx
@@ -1,5 +1,5 @@
 import { forwardRef } from "react";
-import { Modal, Button } from "@navikt/ds-react";
+import { Modal, Button, Accordion } from "@navikt/ds-react";
 
 interface SporingsModalProps {
     selectedItem: { name: string; id: string };
@@ -8,12 +8,47 @@ interface SporingsModalProps {
 const SporingsModal = forwardRef<HTMLDialogElement, SporingsModalProps>(({ selectedItem }, ref) => (
     <Modal ref={ref} header={{ heading: "Sporingskode: " + selectedItem.name }}>
         <Modal.Body>
-            <pre style={{ whiteSpace: 'pre-wrap', wordWrap: 'break-word' }}>
-                <code>
-                    &lt;script defer src="https://cdn.nav.no/team-researchops/sporing/sporing.js"
-                            data-host-url="https://umami.nav.no" data-website-id="{selectedItem.id}"&gt;&lt;/script&gt;
-                </code>
-            </pre>
+            <Accordion>
+                <Accordion.Item defaultOpen>
+                    <Accordion.Header>Standard Umami sporingskode</Accordion.Header>
+                    <Accordion.Content>
+                        <pre style={{whiteSpace: 'pre-wrap', wordWrap: 'break-word'}}>
+                            <code>
+                                &lt;script defer src="https://cdn.nav.no/team-researchops/sporing/sporing.js"
+                                        data-host-url="https://umami.nav.no" data-website-id="{selectedItem.id}"&gt;&lt;/script&gt;
+                            </code>
+                        </pre>
+                    </Accordion.Content>
+                </Accordion.Item>
+                <Accordion.Item>
+                    <Accordion.Header>
+                        Sporingskode for Astro.js apper
+                    </Accordion.Header>
+                    <Accordion.Content>
+                        <pre style={{ whiteSpace: 'pre-wrap', wordWrap: 'break-word' }}>
+                            <code>
+                                &lt;script defer data-astro-rerun src="https://cdn.nav.no/team-researchops/sporing/sporing.js"
+                                        data-host-url="https://umami.nav.no" data-website-id="{selectedItem.id}"&gt;&lt;/script&gt;
+                            </code>
+                        </pre>
+                    </Accordion.Content>
+                </Accordion.Item>
+                <Accordion.Item>
+                    <Accordion.Header>Sporingskode for React Vite.js apper</Accordion.Header>
+                    <Accordion.Content>
+                        <pre style={{ whiteSpace: 'pre-wrap', wordWrap: 'break-word' }}>
+                            <code>
+                                 {`import { Helmet } from "react-helmet";`}
+                                <br/>
+                                &lt;Helmet&gt;<br/>
+                                &lt;script defer src="https://cdn.nav.no/team-researchops/sporing/sporing.js"
+                                            data-host-url="https://umami.nav.no" data-website-id="{selectedItem.id}"&gt;&lt;/script&gt;
+                                <br/>&lt;/Helmet&gt;
+                            </code>
+                        </pre>
+                    </Accordion.Content>
+                </Accordion.Item>
+            </Accordion>
         </Modal.Body>
         <Modal.Footer>
             <Button type="button" onClick={() => (ref as React.RefObject<HTMLDialogElement>).current?.close()}>
