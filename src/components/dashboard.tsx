c3c9b78621b3ec989880a995bd4d1bb57890f643
EIlif Johansen <eilifjohansen@outlook.com>
2024-11-29 22:00:17 +0100

    La til søk på forside

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -1,22 +1,22 @@
-import { Search, ReadMore, List, Alert } from "@navikt/ds-react";
-import { useEffect, useState } from "react";
+import { Search, Alert } from "@navikt/ds-react";
+import { useState } from "react";
 
-interface Website {
+/*interface Website {
     id: string;
     name: string;
     domain: string;
     shareId: string;
     teamId: string;
     createdAt: string;
-}
+}*/
 
 function Dashboard() {
     const baseUrl = window.location.hostname === 'localhost' ? 'https://reops-proxy.intern.nav.no' : 'https://reops-proxy.ansatt.nav.no';
-    const [filteredData, setFilteredData] = useState<Website[] | null>(null);
+   // const [filteredData, setFilteredData] = useState<Website[] | null>(null);
     const [searchQuery, setSearchQuery] = useState<string>('');
     const [alertVisible, setAlertVisible] = useState<boolean>(false);
 
-    useEffect(() => {
+/*    useEffect(() => {
         Promise.all([
             fetch(`${baseUrl}/umami/api/teams/aa113c34-e213-4ed6-a4f0-0aea8a503e6b/websites`, {
                 credentials: window.location.hostname === 'localhost' ? 'omit' : 'include'
@@ -32,29 +32,58 @@ function Dashboard() {
                 setFilteredData(combinedData.filter(item => item.teamId === 'aa113c34-e213-4ed6-a4f0-0aea8a503e6b'));
             })
             .catch(error => console.error("Error fetching data:", error));
-    }, []);
+    }, []);*/
 
     const handleSearchChange = (value: string) => {
         setSearchQuery(value);
         setAlertVisible(false);
     };
 
-    const handleSubmit = (event: React.FormEvent) => {
+    const [searchError, setSearchError] = useState<string | null>(null);
+
+    const handleSubmit = async (event: React.FormEvent) => {
         event.preventDefault();
-        const url = new URL(searchQuery);
+        if (!searchQuery) {
+            setSearchError("Du må sette inn en URL-adresse.");
+            return;
+        }
+        setSearchError(null);
+
+        let inputUrl = searchQuery;
+        if (!inputUrl.startsWith('http://') && !inputUrl.startsWith('https://')) {
+            inputUrl = 'https://' + inputUrl;
+        }
+        const url = new URL(inputUrl);
         const domain = url.hostname;
         const path = encodeURIComponent(url.pathname);
 
-        const matchedWebsite = filteredData?.find(item => item.domain === domain);
+        try {
+            const [data1, data2] = await Promise.all([
+                fetch(`${baseUrl}/umami/api/teams/aa113c34-e213-4ed6-a4f0-0aea8a503e6b/websites`, {
+                    credentials: window.location.hostname === 'localhost' ? 'omit' : 'include'
+                }).then(response => response.json()),
+                fetch(`${baseUrl}/umami/api/teams/bceb3300-a2fb-4f73-8cec-7e3673072b30/websites`, {
+                    credentials: window.location.hostname === 'localhost' ? 'omit' : 'include'
+                }).then(response => response.json())
+            ]);
 
-        if (matchedWebsite) {
-            const umamiUrl = `https://umami.ansatt.nav.no/share/${matchedWebsite.shareId}/${domain}?url=${path}`;
-            window.location.href = umamiUrl;
-        } else {
-            setAlertVisible(true);
+            const combinedData = [...data1.data, ...data2.data];
+            combinedData.sort((a, b) => a.domain.localeCompare(b.domain));
+
+            const filteredData = combinedData.filter(item => item.teamId === 'aa113c34-e213-4ed6-a4f0-0aea8a503e6b');
+            const matchedWebsite = filteredData.find(item => item.domain === domain);
+
+            if (matchedWebsite) {
+                const umamiUrl = `https://umami.ansatt.nav.no/share/${matchedWebsite.shareId}/${domain}?url=${path}`;
+               /* window.location.href = umamiUrl; */
+                window.open(umamiUrl, '_blank');
+            } else {
+                setAlertVisible(true);
+            }
+        } catch (error) {
+            console.error("Error fetching data:", error);
         }
     };
-
     return (
         <div>
             <form role="search" onSubmit={handleSubmit}>
@@ -64,12 +93,13 @@ function Dashboard() {
                         hideLabel={false}
                         variant="primary"
                         value={searchQuery}
+                        error={searchError}
                         onChange={handleSearchChange}
                         onClear={() => setSearchQuery("")}
                     />
                 {alertVisible && <Alert style={{ marginTop: "20px" }} variant="warning">Denne siden har ikke fått støtte for Umami enda. Fortvil ikke — kontakt Team ResearchOps for å få lagt den til :)</Alert>}
                 </div>
-                    <ReadMore style={{ marginTop: "10px" }} header="Hvilke nettsider støttes?">
+{/*                    <ReadMore style={{ marginTop: "10px" }} header="Hvilke nettsider støttes?">
                     <List as="ul">
                         {filteredData && filteredData.map(item => (
                             <List.Item key={item.id}>
@@ -77,7 +107,7 @@ function Dashboard() {
                             </List.Item>
                         ))}
                     </List>
-                </ReadMore>
+                </ReadMore>*/}
             </form>
         </div>
     );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,18 @@
 import {BodyShort, Heading, Link} from "@navikt/ds-react";
 import TeamWebsites from "../components/teamwebsites.tsx";
+import Dashboard from "../components/dashboard.tsx";
 
 function Home() {
     return (
         <>
-                <Heading spacing={true} as="h2" size="large" style={{ marginTop: "60px" }}>Nettsider</Heading>
-                <BodyShort size="medium" style={{ marginBottom: "60px" }}>
+            <Heading spacing={true} as="h2" size="large" style={{ marginTop: "60px" }}>Webstatistikk</Heading>
+            <Dashboard/>
+
+            <Heading spacing={true} as="h3" size="medium" style={{ marginTop: "63px" }}>Nettsider med Umami-støtte</Heading>
+                <BodyShort size="medium" style={{ marginBottom: "30px" }}>
                     Kontakt <Link target="_blank" href={`https://teamkatalog.nav.no/team/26dba481-fd96-40a8-b47d-b1ad0002bc74`}>Team ResearchOps</Link> for å få sporingskode til din nettside.
                 </BodyShort>
-
-                <TeamWebsites/>
+            <TeamWebsites/>
         </>
     )
 }
