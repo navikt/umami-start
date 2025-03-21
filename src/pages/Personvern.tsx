e8b3f6d89ef434222878f2f3f9c0356d5ca495d9
EIlif Johansen <eilifjohansen@outlook.com>
2025-03-21 10:53:18 +0100

    Update Personvern.tsx

--- src/pages/Personvern.tsx
+++ src/pages/Personvern.tsx
@@ -17,8 +17,26 @@ function Personvern() {
                         </p>
                         <p className="mb-4 leading-normal">Vi bruker informasjonskapsler til å forbedre brukeropplevelsen og innholdet. Når du besøker aksel.nav.no, sender nettleseren din opplysninger til NAVs analyseverktøy. For hver side du åpner, lagres opplysninger om hvilken side du er på, hvilken side du kommer fra og går til, hvilken nettleser du bruker, om du bruker PC eller mobile løsninger m.m. Slik kan vi forbedre flyten og opplevelsen for alle som bruker nettsiden.
                         </p>
-                        <p className="mb-4 leading-normal">Opplysningene brukes til å kartlegge hvordan og hvor mye nettsiden brukes, uten å identifisere IP-adresser. Vi bruker verktøyet Umami i analysearbeidet.
+                        <p className="mb-4 leading-normal">Opplysningene brukes til å kartlegge hvordan og hvor mye nettsiden brukes, uten å identifisere IP-adresser. Vi bruker verktøyet Umami og Skyra i analysearbeidet.
                         </p>
+
+                        <h3 className="pb-4 text-lg">skyra*</h3>
+                        <p className="mb-4 leading-normal">
+                        Brukes av verktøyet Skyra for å lagre svarene du gir på en undersøkelse. Avhengig av oppsett på undersøkelsen er dette enten en sesjonskapsel eller en kapsel som slettes etter få timer. Kapselen slettes når en undersøkelse fullføres eller lukkes.
+                        </p>
+                        
+                        <h3 className="pb-4 text-lg">skyra.state</h3>
+
+                        <p className="mb-4 leading-normal">
+                        Brukes av verktøyet Skyra for å huske brukeren og hvorvidt undersøkelser er åpnet, lukket eller fullført.
+                        </p>
+
+                        <h3 className="pb-4 text-lg">Umami</h3>
+
+                        <p className="mb-4 leading-normal">
+                        Umami brukes til statistikk og analyse av hvordan nav.no brukes. Unami bruker ikke informasjonskapsler, men henter inn opplysninger om nettleseren din for å lage en unik ID. Denne ID-en brukes for å skille deg fra andre brukere. For å hindre identifisering, bruker vi en egenutviklet proxy som vasker bort deler av IP-adressen din før dataene sendes til verktøyet.
+                        </p>
+
                         <h2 className="pb-4 pt-4 text-2xl">Feil, mangler og forbedringsforslag</h2>
                         <p className="leading-normal">Hvis du opplever problemer eller har forslag til forbedringer hører vi veldig gjerne fra deg! Feil og mangler kan rapporteres til <a className="underline" href="mailto:eilif.johansen@nav.no">eilif.johansen@nav.no</a>, eller <a className="underline" href="https://nav-it.slack.com/archives/C02UGFS2J4B">#researchops</a> på Slack.</p>
                     </div>
