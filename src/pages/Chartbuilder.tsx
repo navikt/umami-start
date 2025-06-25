3e95ec6e3377e13222517f11931d566045126676
EIlif Johansen <eilifjohansen@outlook.com>
2025-06-25 16:58:47 +0200

    Språkjustering

--- src/pages/Chartbuilder.tsx
+++ src/pages/Chartbuilder.tsx
@@ -1385,7 +1385,7 @@ const ChartsPage = () => {
   return (
     <div className="w-full max-w-[1600px]">
       <Heading spacing level="1" size="medium" className="pt-12 pb-4">
-        Umami Grafbyggeren for Metabase
+        Umami grafbygger for Metabase
       </Heading>
       <Heading level="3" size="small" spacing className="text-gray-700 mt-2 mb-3">
         Lurer du på hvordan folk bruker nettsiden eller appen din?
