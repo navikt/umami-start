fef46d4dfcd023ab17a366dc9b694875c9fa3f41
EIlif Johansen <eilifjohansen@outlook.com>
2026-01-12 10:25:31 +0100

    Start Umami får logo

--- src/components/theme/Header/Header.tsx
+++ src/components/theme/Header/Header.tsx
@@ -29,8 +29,25 @@ export default function Header() {
       <header className="flex py-1 z-10 items-center max-w-[76.5rem] m-auto justify-between">
         <div className="flex items-stretch">
           <Link className={linkButton} href="/">
+            <svg
+              width="1em"
+              height="1em"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="text-2xl mr-1"
+              aria-hidden="true"
+            >
+              <path
+                d="M5 4L19 12L5 20V4Z"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
             <span className="text-2xl whitespace-nowrap text-white">
-              Start Umami
+              Umami
             </span>
           </Link>
         </div>
