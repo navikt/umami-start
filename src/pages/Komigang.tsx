d45c8914486ffb108442545f68dc8e4e0891cef6
EIlif Johansen <eilifjohansen@outlook.com>
2026-01-17 16:43:52 +0100

    Språkfiks

--- src/pages/Komigang.tsx
+++ src/pages/Komigang.tsx
@@ -28,6 +28,9 @@ function Komigang() {
                             <strong>Cookie-samtykke:</strong> Håndteres automatisk av dekoratøren.
                         </List.Item>
                         <List.Item>
+                            <strong>Hva spores:</strong> Dekoratøren sporer grunnleggende sidenavigasjon og metadata. <Link href="https://navno.sharepoint.com/sites/intranett-utvikling/SitePages/Rutine-for-bruk-av-Umami.aspx#hva-sporer-umami">Se hva Umami sporer</Link>.
+                        </List.Item>
+                        <List.Item>
                             <Link href={"https://github.com/navikt/nav-dekoratoren-moduler/blob/main/README.md#getanalyticsinstance"}>
                                 Se dokumentasjon for sporing til dekoratøren
                             </Link>
@@ -40,7 +43,7 @@ function Komigang() {
                         Bruker du ikke dekoratøren?
                     </Heading>
                     <BodyShort spacing>
-                        Kontakt <Link target="_blank" href={"https://nav-it.slack.com/archives/C02UGFS2J4B"}>#ResearchOps på Slack</Link> for å få sporingskode til nettsiden eller appen din.
+                        Kontakt <Link target="_blank" href={"https://nav-it.slack.com/archives/C02UGFS2J4B"}>#ResearchOps på Slack</Link> for å få sporingskode til nettsiden eller appen din. Du kan også se <Link href={"/oppsett"}>eksisterende sporingskoder nederst på oppsett-siden</Link>.
                     </BodyShort>
                 </div>
             </div>
@@ -53,11 +56,20 @@ function Komigang() {
             <Heading spacing={true} as="h3" size="medium"
                 style={{ marginTop: "60px", marginBottom: "30px" }}>Få klarsignal for Umami i produksjon</Heading>
 
-            <BodyShort>
-                <strong>Etterlevelse:</strong> <Link
-                    href={"https://etterlevelse.ansatt.nav.no/dokumentasjon/e3757864-9720-4569-9e8e-50841950fcd6"}>Fyll ut
-                    gjenbrukbar Umami etterlevelsesdokument.</Link>
+            <BodyShort spacing>
+                Før du tar Umami i bruk i produksjon må du sørge for at personvernet ivaretas. Personopplysninger skal ikke spores tilsiktet i Umami.
             </BodyShort>
+
+            <List as="ul">
+                <List.Item>
+                    <strong>Etterlevelse:</strong> <Link
+                        href={"https://etterlevelse.ansatt.nav.no/dokumentasjon/e3757864-9720-4569-9e8e-50841950fcd6"}>Fyll ut
+                        gjenbrukbar Umami etterlevelsesdokument</Link>. Du må også opplyse om bruk av Umami via personvernsidene på nettsiden/appen.
+                </List.Item>
+                <List.Item>
+                    <strong>Personvern:</strong> Umami-proxyen vasker automatisk vekk personopplysninger som fødselsnummer, e-post, telefonnummer m.m. <Link href="https://navno.sharepoint.com/sites/intranett-utvikling/SitePages/Rutine-for-bruk-av-Umami.aspx#hva-vasker-vi-vekk-fra-umami">Se hva som vaskes vekk</Link>.
+                </List.Item>
+            </List>
 
             {/*                <List as="ul" title="Slik får du tommel opp fra juristen">
                     <List.Item>
@@ -95,9 +107,13 @@ function Komigang() {
                 </List.Item>
                 <List.Item>
                     <strong>Målesjekk:</strong> Sjekk at dataene blir samlet inn riktig. Gjør en målesjekk på nettsiden
-                    eller appen din.
+                    eller appen din. Husk at teamet har ansvar for dataene som samles inn.
                 </List.Item>
             </List>
+
+            <BodyShort spacing className="mt-4">
+                <strong>Viktig:</strong> Ikke spor innhold fra fritekstfelt eller søkefelt, da dette kan inneholde personopplysninger. Unntak krever egne vurderinger av personvernkonsekvenser (PVK) og ekstra tiltak. Vær oppmerksom på risikoen for avvik. <Link href="https://navno.sharepoint.com/sites/intranett-utvikling/SitePages/Rutine-for-bruk-av-Umami.aspx#hva-skal-ikke-spores-i-umami">Les mer om hva som ikke skal spores</Link>.
+            </BodyShort>
 
             <Heading spacing={true} as="h3" size="medium"
                 style={{ marginTop: "60px", marginBottom: "30px" }}>Analysere</Heading>
