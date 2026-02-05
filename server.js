2dc788d99cd0604bea5dc930ec083c9b6957f6a1
Kenneth Isaksen <kenneth.bakke.isaksen@nav.no>
2026-02-05 10:19:26 +0100

    chore: proxy url based on env

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ import dotenv from 'dotenv'
 import { BigQuery } from '@google-cloud/bigquery'
 
 const BIGQUERY_TIMEZONE = 'Europe/Oslo';
-const SITEIMPROVE_PROXY_BASE_URL = process.env.SITEIMPROVE_PROXY_BASE_URL
-    || (process.env.NAIS_CLUSTER_NAME?.includes('dev')
-        ? 'https://reops-proxy.intern.dev.nav.no/siteimprove'
-        : 'https://reops-proxy.intern.nav.no/siteimprove');
+const SITEIMPROVE_BASE_URL = process.env.SITEIMPROVE_BASE_URL;
+
+if (!SITEIMPROVE_BASE_URL) {
+    throw new Error('Missing env var: SITEIMPROVE_BASE_URL');
+}
 
 dotenv.config()
 
