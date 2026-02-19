422a5f1007f01fc293a1ed07467d64f2aa0d3017
Kenneth Isaksen <kenneth.bakke.isaksen@nav.no>
2026-02-19 09:46:01 +0100

    chore: funnel feature structure

--- src/client/features/funnel/index.ts
+++ src/client/features/funnel/index.ts
@@ -1,2 +1,15 @@
 // UI Components
 export { default as Funnel } from './ui/Funnel';
+
+// Types
+export type { StepParam, FunnelStep, QueryStats, FunnelResultRow, TimingResultRow, FunnelApiResponse, TimingApiResponse, EventsApiResponse } from './model/types';
+
+// Hooks
+export { useFunnel } from './hooks/useFunnel';
+
+// API
+export { fetchFunnelData, fetchTimingData, fetchWebsiteEvents } from './api/funnelApi';
+
+// Utils
+export { formatDuration, downloadCSV, copyToClipboard, generateMetabaseFunnelSql, generateMetabaseTimingSql } from './utils/funnelUtils';
+export { parseStepsFromParams, addStep, removeStep, updateStepValue, updateStepType, updateStepEventScope, addStepParam, removeStepParam, updateStepParam, normalizeStepUrl } from './utils/stepUtils';
