4c4a2aa9a4fe0a1c9520d2c196bab8b6f924c61a
EIlif Johansen <eilifjohansen@outlook.com>
2026-02-27 02:40:41 +0100

    Språkfiks

--- src/client/features/chartbuilder/ui/grafbygger/EventSelector.tsx
+++ src/client/features/chartbuilder/ui/grafbygger/EventSelector.tsx
@@ -177,7 +177,7 @@ const EventSelector = ({
   return (
     <div className='mb-4'>
       <Heading level="3" size="xsmall" spacing className="mt-6">
-        Velg hendelse
+        Hendelser
       </Heading>
 
       <div className="mt-3 bg-(--ax-bg-default) p-4 rounded-md border shadow-inner">
